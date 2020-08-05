b2dd6af7849705258d32ba830740054f389d80eb
Milkyas Afework <60724009+milkyasafework@users.noreply.github.com>
2020-08-05 12:31:42 -0500

    jha

--- common-js/video-hover.js
+++
@@ -1,9 +0,0 @@
-var $polystar = $('.polystar');
-
-$polystar.on('mouseenter focus', function () {
-  $polystar.get(0).play();
-});
-
-$polystar.on('mouseleave blur', function () {
-  $polystar.get(0).pause();
-});
